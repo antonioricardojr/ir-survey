a30c08ad01500eed7861872638b4483c48f24a3e
Antonio Marques <antonioricardojr@gmail.com>
2018-06-10 14:54:53 -0300

    adding next item to evaluate

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,14 +1,26 @@
 var app = angular.module( 'app' );
 
-
-
 app.controller( 'MainController', [ '$scope', '$http', function ( $scope, $http ) {
 
     var self = this;
 
+    self.idx = 0
+
     self.items = $http.get( '../../docs.json' )
         .then( function ( res ) {
             self.items = res.data;
+            self.item_to_evaluate = self.items[ self.idx ]
+            console.log( self.items )
         } );
+
+    self.next = function ( ) {
+
+        if ( self.idx < self.items.length - 1 ) {
+            self.idx += 1
+        } else {
+            self.idx = 0
+        }
+        self.item_to_evaluate = self.items[ self.idx ]
+    }
 
 } ] );
